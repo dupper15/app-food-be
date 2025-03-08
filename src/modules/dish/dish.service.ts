ea249997325e5212b5628cd39ff2d5598e27ad92
highlamjay <caoduonglam61@gmail.com>
2025-03-08 14:41:11 +0700

    feat: api dish and check api order-item

--- src/modules/dish/dish.service.ts
+++ src/modules/dish/dish.service.ts
@@ -1,9 +1,10 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Dish } from './dish.schema';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 import { CreateDishDto } from './dto/createDish.dto';
 import { Restaurant } from '../restaurant/restaurant.schema';
+import { EditToppingDto } from '../topping/dto/editTopping.dto';
 
 @Injectable()
 export class DishService {
@@ -23,5 +24,65 @@ export class DishService {
     }
     const newDish = new this.dishModel(createDishDto);
     return newDish.save();
+  }
+
+  async editDish(id: ObjectId, editDishDto: EditToppingDto): Promise<Dish> {
+    const updatedDish = await this.dishModel.findByIdAndUpdate(
+      id,
+      editDishDto,
+      { new: true, runValidators: true },
+    );
+
+    //check updated dish
+    if (!updatedDish) {
+      throw new BadRequestException('Dish not found');
+    }
+
+    return updatedDish;
+  }
+
+  async deleteDish(id: ObjectId): Promise<{ msg: string }> {
+    //find dish to delete
+    const deletedDish = await this.dishModel.findById(id);
+
+    //check dish
+    if (!deletedDish) {
+      throw new BadRequestException('Dish not found');
+    }
+
+    //delete
+    await this.dishModel.findByIdAndDelete(id);
+
+    return { msg: 'Dish deleted successfully' };
+  }
+
+  async fetchAllDishByRestaurant(id: ObjectId): Promise<Dish[]> {
+    return await this.dishModel.find({ restaurant_id: id }).exec();
+  }
+
+  async fetchDetailDish(id: ObjectId): Promise<Dish> {
+    //check dish
+    const checkDish = await this.dishModel.findById(id);
+    if (!checkDish) {
+      throw new BadRequestException('Dish not found');
+    }
+    return checkDish;
+  }
+
+  async fetchAllDishCategoryByRestaurant(
+    restaurant_id: ObjectId,
+    category_id: ObjectId,
+  ): Promise<Dish[]> {
+    const dishByRestaurant = await this.dishModel.find({
+      restaurant_id: restaurant_id,
+      category_id: category_id,
+    });
+    if (!dishByRestaurant || dishByRestaurant.length === 0) {
+      throw new BadRequestException(
+        'No dishes found for this category in the specified restaurant',
+      );
+    }
+
+    return dishByRestaurant;
   }
 }
