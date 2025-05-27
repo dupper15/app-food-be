a692e69fbb89332868c5aeaa7370f9c2b60bcb04
highlamjay <caoduonglam61@gmail.com>
2025-05-27 11:31:42 +0700

    fix: upload image in rating

--- src/modules/reply/reply.controller.ts
+++ src/modules/reply/reply.controller.ts
@@ -1,17 +1,37 @@
-import { Controller, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Param,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
 import { ReplyService } from './reply.service';
 import { Reply } from './reply.schema';
 import { CreateReplyDto } from './dto/createReply.dto';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { UploadService } from '../upload/upload.service';
 
 @Controller('reply')
 export class ReplyController {
-  constructor(private readonly replyService: ReplyService) {}
+  constructor(
+    private readonly replyService: ReplyService,
+    private readonly uploadService: UploadService,
+  ) {}
 
   @Post(':id/create')
+  @UseInterceptors(FilesInterceptor('images'))
   async createReply(
     @Param('id') id: string,
     @Body() data: CreateReplyDto,
+    @UploadedFile() imagesUpload: Express.Multer.File[],
   ): Promise<Reply> {
-    return this.replyService.createReply(id, data);
+    let imageUrls: string[] = [];
+    if (imagesUpload?.length) {
+      imageUrls = await this.uploadService.uploadMultipleImages(imagesUpload);
+      data.images = imageUrls;
+    }
+
+    return await this.replyService.createReply(id, data);
   }
 }
